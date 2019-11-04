ca1dde7e3c89e0b00c3571dc42528a3ac11a37e6
Werner <wernero@live.co.za>
2019-11-04 09:17:00 +0200

    Images and Files added
    To test attachments with

--- features/warehouse/Portal_warehouse.rb
+++ features/warehouse/Portal_warehouse.rb
@@ -2,4 +2,11 @@
 
 class ElementWarehouse
 	WIDGET = { xpath: '//*[@id="higuru-webchat"]/div/div/img' }
+	GET_STARTED_REPLY = { xpath: '//*[@id="higuru-webchat"]/div/div[1]/div[2]/div[3]/div[2]/button[1]' }
+	SUPPORT_REPLY = { xpath: '//*[@id="higuru-webchat"]/div/div[1]/div[2]/div[3]/div[2]/button[2]' }
+	PRODUCT_INFO_REPLY = { xpath: '//*[@id="higuru-webchat"]/div/div[1]/div[2]/div[3]/div[2]/button[3]' }
+	FEEDBACK_REPLY = { xpath: '//*[@id="higuru-webchat"]/div/div[1]/div[2]/div[3]/div[2]/button[4]' }
+	CHAT_WITH_US_REPLY = { xpath: '//*[@id="higuru-webchat"]/div/div[1]/div[2]/div[3]/div[2]/button[5]' }
+	ATTACH_FILE_BUTTON = { xpath: '//*[@id="higuru-webchat"]/div/div[1]/div[3]/div[2]/label/svg/g' }
+	CHAT_CLOSE_BUTTON = { xpath: '//*[@id="higuru-webchat"]/div/div[1]/div[1]/div[1]/svg/path'}
 end
