19c40c671981573e246054826ba86d38c157d5d9
Werner <wernero@live.co.za>
2019-11-18 14:20:29 +0200

    Step_update

--- features/step_definitions/Sign_In_Steps.rb
+++ features/step_definitions/Sign_In_Steps.rb
@@ -2,18 +2,18 @@
 
 Given('I am on the Sign In Page') do
 	$web_driver.find_element(ElementWarehouse::WIDGET).click
-	visa = $web_driver.find_element(xpath: '/html/body/div[2]/div/div[1]/div[2]/div[3]/div[1]/div/div/div[1]')
-	expect(visa.text).to include('Hi! Welcome to hi.guru.')
-	visa = $web_driver.find_element(xpath: '/html/body/div[2]/div/div[1]/div[2]/div[3]/div[1]/div/div/div[1]')
-	expect(visa.text).to include('Let’s boost')
-	visa = $web_driver.find_element(xpath: '/html/body/div[2]/div/div[1]/div[2]/div[3]/div[1]/div/div/div[1]')
-	expect(visa.text).to include('your business growth and customer')
-	visa = $web_driver.find_element(xpath: '/html/body/div[2]/div/div[1]/div[2]/div[3]/div[1]/div/div/div[1]')
-	expect(visa.text).to include('engagement!Excited? So am I! Let’s')
-	visa = $web_driver.find_element(xpath: '/html/body/div[2]/div/div[1]/div[2]/div[3]/div[1]/div/div/div[1]')
-	expect(visa.text).to include('get chatting, how can I assist you')
-	visa = $web_driver.find_element(xpath: '/html/body/div[2]/div/div[1]/div[2]/div[3]/div[1]/div/div/div[1]')
-	expect(visa.text).to include('today?')
+	robot = $web_driver.find_element(xpath: '/html/body/div[2]/div/div[1]/div[2]/div[3]/div[1]/div/div/div[1]')
+	expect(robot.text).to include('Hi! Welcome to hi.guru.')
+	robot = $web_driver.find_element(xpath: '/html/body/div[2]/div/div[1]/div[2]/div[3]/div[1]/div/div/div[1]')
+	expect(robot.text).to include('Let’s boost')
+	robot = $web_driver.find_element(xpath: '/html/body/div[2]/div/div[1]/div[2]/div[3]/div[1]/div/div/div[1]')
+	expect(robot.text).to include('your business growth and customer')
+	robot = $web_driver.find_element(xpath: '/html/body/div[2]/div/div[1]/div[2]/div[3]/div[1]/div/div/div[1]')
+	expect(robot.text).to include('engagement!Excited? So am I! Let’s')
+	robot = $web_driver.find_element(xpath: '/html/body/div[2]/div/div[1]/div[2]/div[3]/div[1]/div/div/div[1]')
+	expect(robot.text).to include('get chatting, how can I assist you')
+	robot = $web_driver.find_element(xpath: '/html/body/div[2]/div/div[1]/div[2]/div[3]/div[1]/div/div/div[1]')
+	expect(robot.text).to include('today?')
 end
 
 When('I Sign in with Invalid Credentials') do
