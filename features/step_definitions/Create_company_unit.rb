4d91325b6ead55345663a3a165f88f53489323b8
Werner Oosthuizen <44438817+werneroos@users.noreply.github.com>
2020-01-10 13:31:44 +0200

    Update Create_company_unit.rb

--- features/step_definitions/Create_company_unit.rb
+++ features/step_definitions/Create_company_unit.rb
@@ -6,6 +6,6 @@ Then('I create the company unit') do
 	$web_driver.find_element(ElementWarehouse::CREATE).click
 end
 
-Then('I click on Company unit') do
+Then('I confirm the Company unit has been created') do
 	$web_driver.find_element(ElementWarehouse::COMPANY_UNIT).click
 end
