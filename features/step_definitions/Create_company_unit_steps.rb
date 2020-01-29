59c2de982e7565cf782eba3621a3102a3386d1d8
Werner <wernero@fsatcomm.com>
2020-01-29 17:41:29 +0200

    Rename Steps

--- features/step_definitions/Create_company_unit_steps.rb
+++ features/step_definitions/Create_company_unit_steps.rb
@@ -1,6 +1,6 @@
 Then('I create the company unit') do
 	$web_driver.find_element(ElementWarehouse::COMPANY_UNIT).click
-	sleep 3
+	sleep 4
 	$web_driver.find_element(ElementWarehouse::ADD_COMPANY_UNIT).click
 	$web_driver.find_element(ElementWarehouse::COMPANY_UNIT_INPUT).send_keys('Company Unit 2')
 	$web_driver.find_element(ElementWarehouse::CREATE).click
