6d8e4f1a6a743ff93cb83b58ac99a7a77b35cd7b
Werner <wernero@fsatcomm.com>
2020-02-05 14:47:23 +0200

    Space in seconds

--- features/step_definitions/Bot_Widget_Steps.rb
+++ features/step_definitions/Bot_Widget_Steps.rb
@@ -368,18 +368,18 @@ Then('I get feedback from the agent') do
 	$stdout.flush
 	check_for_reply
 	TIME_1 = Time.now - TIME
-	$stdout.puts('Total time of first response = ' + TIME_1.to_s + 'Seconds')
+	$stdout.puts('Total time of first response = ' + TIME_1.to_s + ' Seconds')
 	$stdout.flush
 	$web_driver.find_element(ElementWarehouse::CHAT_TEXT_FIELD).send_keys('Thank you. This is a Test. Please can we Resolve this query')
 	$web_driver.find_element(ElementWarehouse::SEND_BUTTON).click
 	check_for_reply_2
 	TIME_2 = Time.now - TIME
-	$stdout.puts('Total time of second response = ' + TIME_2.to_s + 'Seconds')
+	$stdout.puts('Total time of second response = ' + TIME_2.to_s + ' Seconds')
 	$stdout.flush
 	check_for_reply_3
 	RESOLVED_TIME = $web_driver.find_element(ElementWarehouse::RESOLVED_TIME).text
 	$stdout.puts("Resolved Time = " + RESOLVED_TIME)
 	TIME_3 = Time.now - TIME
-	$stdout.puts('Total time of conversation = ' + TIME_3.to_s + 'Seconds')
+	$stdout.puts('Total time of conversation = ' + TIME_3.to_s + ' Seconds')
 	$stdout.flush
 end
