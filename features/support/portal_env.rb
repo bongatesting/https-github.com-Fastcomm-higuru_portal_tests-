d6988e0f9caf7ce3c616919b24037808a545d2f7
Werner <wernero@fsatcomm.com>
2020-06-22 15:34:46 +0200

    Notifications enabled

--- features/support/portal_env.rb
+++ features/support/portal_env.rb
@@ -16,9 +16,16 @@ require 'watir-scroll'
 ENV['HOST'] = 'app-qa' if ENV['HOST'].nil?
 
 Before do # Chrome Env
+  preferences = { "profile.managed_default_content_settings.notifications" => 1,
+                  "profile.managed_default_content_settings.images" => 1,
+                  "profile.managed_default_content_settings.popups" => 1
+  }
+  'chromeOptions' == {
+      :prefs => preferences,
+  }
   options = Selenium::WebDriver::Chrome::Options.new
   #options.add_argument('--headless') # configure the driver to run in headless mode
-  $web_driver = Selenium::WebDriver.for :chrome, options: options
+  $web_driver = Selenium::WebDriver.for :chrome, options: options, prefs: preferences
   $web_driver.navigate.to "https://#{ENV['HOST']}.hi.guru/"
   #$web_driver.navigate.to "https://google.com/"
   Selenium::WebDriver::Wait.new(timeout: 10)
