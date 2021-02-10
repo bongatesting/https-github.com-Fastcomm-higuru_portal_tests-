06d697e941552ede47096a4920050a24a204916b
Bonga <bonga@hi.guru>
2021-02-10 14:13:23 +0200

    downloading Transcripts
    downloading Transcripts

---
+++ features/step_definitions/Download_Full_convo_transcripts_steps.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+wait = Selenium::WebDriver::Wait.new(timeout: 80)
+
+Given('I have Conversation history') do
+	$web_driver.find_element(ElementWarehouse::EMAIL_FIELD).send_keys(TestUser.email)
+	sleep 3
+	$web_driver.find_element(ElementWarehouse::PASSWORD_FIELD).send_keys(TestUser.password)
+	sleep 3
+	$web_driver.find_element(ElementWarehouse::LOGIN_BUTTON).click
+	sleep 5
+	$web_driver.find_element(ElementWarehouse::LOGIN_ACCOUNT_SELECT).click
+	sleep 3
+	$web_driver.find_element(ElementWarehouse::COMPANY_UNIT_SELECT).click
+end
+
+Then('I download the Full Conversation Transcripts') do
+	$web_driver.find_element(ElementWarehouse::CONVERSATION_HISTORY_TAB).click
+	sleep 6
+	$web_driver.find_element(ElementWarehouse::OPEN_CONVERSATION).click
+	sleep 3
+	$web_driver.find_element(ElementWarehouse::CONVO_HISTORY_DOWNLOAD_ICON).click
+	sleep 3
+	$web_driver.find_element(ElementWarehouse::FULL_TRANSCRIPTS_BUTTON).click
+	sleep 3
+end
